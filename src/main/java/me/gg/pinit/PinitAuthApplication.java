f427e2971e37863595ac65d80ebb143b8e4f6d92
GoGradually <chkun3109@gmail.com>
2025-12-01 21:11:09 +0900

    chore: 초기 프로젝트 설정 및 Gradle 구성 추가

---
+++ src/main/java/me/gg/pinit/PinitAuthApplication.java
@@ -0,0 +1,13 @@
+package me.gg.pinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinitAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinitAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/gg/pinit/PinitAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gg.pinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinitAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
