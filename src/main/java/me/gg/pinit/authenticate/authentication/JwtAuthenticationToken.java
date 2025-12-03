6cf231a5290942e462aac5280035c446cdc5b6bc
GoGradually <chkun3109@gmail.com>
2025-12-03 18:05:18 +0900

    feat: JwtAuthenticationToken 클래스 추가

---
+++ src/main/java/me/gg/pinit/authenticate/authentication/JwtAuthenticationToken.java
@@ -0,0 +1,45 @@
+package me.gg.pinit.authenticate.authentication;
+
+import jakarta.annotation.Nullable;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class JwtAuthenticationToken extends AbstractAuthenticationToken {
+    private final @Nullable Object principal;
+    private final String token;
+
+    public JwtAuthenticationToken(String token) {
+        super(Collections.emptyList());
+        this.principal = null;
+        this.token = token;
+        setAuthenticated(false);
+    }
+
+    public JwtAuthenticationToken(Object principal, String token, Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.principal = principal;
+        this.token = token;
+        setAuthenticated(true);
+    }
+
+    @Override
+    public @Nullable Object getCredentials() {
+        return token;
+    }
+
+    @Override
+    public @Nullable Object getPrincipal() {
+        return principal;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) {
+        if (isAuthenticated) {
+            throw new IllegalArgumentException("인증 상태 설정은 생성자에서만 할 수 있습니다.");
+        }
+        super.setAuthenticated(false);
+    }
+}
