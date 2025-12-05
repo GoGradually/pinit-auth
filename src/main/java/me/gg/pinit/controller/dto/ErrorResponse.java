a956bc5fe7c92106bdd037acb174ba10ad419534
GoGradually <chkun3109@gmail.com>
2025-12-05 23:15:18 +0900

    feat: ErrorResponse 클래스 추가 및 오류 메시지 반환 메서드 구현

---
+++ src/main/java/me/gg/pinit/controller/dto/ErrorResponse.java
@@ -0,0 +1,12 @@
+package me.gg.pinit.controller.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record ErrorResponse(
+        @Schema(description = "오류 메시지", example = "아이디 또는 비밀번호가 올바르지 않습니다.")
+        String message
+) {
+    public static ErrorResponse of(String message) {
+        return new ErrorResponse(message);
+    }
+}
