c589016377859a482f76d2ba3721b79c384a64fb
GoGradually <chkun3109@gmail.com>
2025-12-05 22:55:04 +0900

    feat: NaverLoginSetting 클래스를 OauthLoginSetting으로 이름 변경 및 Lombok 어노테이션 추가

--- src/main/java/me/gg/pinit/controller/dto/OauthLoginSetting.java
+++ src/main/java/me/gg/pinit/controller/dto/OauthLoginSetting.java
@@ -1,6 +1,11 @@
 package me.gg.pinit.controller.dto;
 
-public class NaverLoginSetting {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OauthLoginSetting {
     private String response_type;
     private String client_id;
     private String redirect_uri;
