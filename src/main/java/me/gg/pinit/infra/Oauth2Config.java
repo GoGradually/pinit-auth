f43964761e951b79a857fe70466b2de42f93bde2
GoGradually <chkun3109@gmail.com>
2025-12-05 18:28:12 +0900

    feat: NaverOauth2Provider와 NaverClient 빈 등록

---
+++ src/main/java/me/gg/pinit/infra/Oauth2Config.java
@@ -0,0 +1,20 @@
+package me.gg.pinit.infra;
+
+import me.gg.pinit.infra.naver.NaverClient;
+import me.gg.pinit.infra.naver.NaverOauth2Provider;
+import me.gg.pinit.infra.naver.NaverRegistrationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Oauth2Config {
+    @Bean
+    public NaverOauth2Provider naverOauth2Provider(NaverRegistrationProperties naverRegistrationProperties) {
+        return new NaverOauth2Provider(naverClient(naverRegistrationProperties));
+    }
+
+    @Bean
+    public NaverClient naverClient(NaverRegistrationProperties naverRegistrationProperties) {
+        return new NaverClient(naverRegistrationProperties);
+    }
+}
