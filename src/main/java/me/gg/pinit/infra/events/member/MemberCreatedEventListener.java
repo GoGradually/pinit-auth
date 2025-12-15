51ffdc0857b01dd845be8ec41a37674f70b1fed5
GoGradually <chkun3109@gmail.com>
2025-12-15 19:52:27 +0900

    feat: MemberCreatedEventListener 클래스 수정 및 아웃박스 서비스 통합

--- src/main/java/me/gg/pinit/infra/events/member/MemberCreatedEventListener.java
+++ src/main/java/me/gg/pinit/infra/events/member/MemberCreatedEventListener.java
@@ -1,14 +1,29 @@
 package me.gg.pinit.infra.events.member;
 
 import me.gg.pinit.domain.event.MemberCreatedEvent;
+import me.gg.pinit.infra.events.outbox.OutboxService;
+import me.gg.pinit.infra.events.rabbitmq.RabbitEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class MemberCreatedEventListener {
 
+    private final ApplicationEventPublisher applicationEventPublisher;
+    private final OutboxService outboxService;
+
+    public MemberCreatedEventListener(ApplicationEventPublisher applicationEventPublisher, OutboxService outboxService) {
+        this.applicationEventPublisher = applicationEventPublisher;
+        this.outboxService = outboxService;
+    }
+
     @EventListener
     public void on(MemberCreatedEvent event) {
-
+        MemberCreatedPayload outbox = new MemberCreatedPayload(event.getMemberId(), event.getNickname(), LocalDateTime.now());
+        outboxService.save(outbox);
+        applicationEventPublisher.publishEvent(new RabbitEvent(outbox.getId()));
     }
 }
